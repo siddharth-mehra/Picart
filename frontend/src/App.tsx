e5a22c8570cac1d61f1b40a3f6ab7bbd39622158
siddharth-mehra <sid15.mehra@gmail.com>
2025-03-16 20:38:15 +0530

    Added background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,27 @@
-
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import SignInPage from "./components/sign-in";
 import SignUpPage from "./components/sign-up";
 import Home from "./components/Home";
+import { GridBackgroundDemo } from "./components/ui/Background";
 
 export default function App() {
   return (
     <Router>
-      <div className="bg-zinc-900 w-full min-h-screen">
-      <div className=" flex flex-col  max-w-[1280px] mx-auto">
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<SignInPage />} />
-          <Route path="/sign-up" element={<SignUpPage/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}></Route>
-        </Routes>
+      <div className="relative w-full min-h-screen items-center justify-center flex dark">
+        <GridBackgroundDemo />
+          <div className="absolute w-full min-h-screen max-w-[1280px] items-center justify-center flex z-0">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignInPage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+          </div>
       </div>
-      </div>
-      
     </Router>
   );
 }
+
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
-
+import { Provider } from 'react-redux'
+import store from './store/index.ts'
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -14,7 +15,9 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ClerkProvider>
   </React.StrictMode>,
 )
